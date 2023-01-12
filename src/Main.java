6ba3282025dd3d7a979b1c86628debabaca3f6be
Eduardo <edumoraesdg@gmail.com>
2023-01-12 16:42:44 -0400

    Criando request HTTP e imprimindo no console

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create(System.getenv("URI_API")))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
