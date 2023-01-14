ad5c0ee2f0639f48fc3024131daa30583467d9dd
Eduardo <edumoraesdg@gmail.com>
2023-01-14 03:34:44 -0400

    Parseando resposta da API

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -13,6 +20,28 @@ public class Main {
                 .GET()
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JsonNode responseNode = objectMapper
+                .readTree(response.body())
+                .path("items"); //parse to Jackson JsonNode
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); //Exception double quote
+
+        CollectionType collectionType = TypeFactory
+                .defaultInstance()
+                .constructCollectionType(List.class, Movie.class);
+
+        List<Movie> listMovies = objectMapper
+                .reader(collectionType)
+                .readValue(responseNode);
+
+        for (int i = 0; i < listMovies.size(); i++) {
+            String title = listMovies.get(i).getTitle();
+            String rank = listMovies.get(i).getRank();
+            System.out.println(rank + " " + title);
+        }
+
     }
 }
---
+++ src/Movie.java
@@ -0,0 +1,73 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Movie {
+
+    private String id;
+    private String rank;
+    private String title;
+    private String fullTitle;
+    private String year;
+    private String image;
+    private String crew;
+    private String imDbRating;
+    private String imDbRatingCount;
+
+    @JsonCreator
+    public Movie(
+            @JsonProperty("id") final String id,
+            @JsonProperty("rank") final String rank,
+            @JsonProperty("title") final String title,
+            @JsonProperty("fullTitle") final String fullTitle,
+            @JsonProperty("year") final String year,
+            @JsonProperty("image") final String image,
+            @JsonProperty("crew") final String crew,
+            @JsonProperty("imDbRating") final String imDbRating,
+            @JsonProperty("imDbRatingCount") final String imDbRatingCount) {
+        this.id = id;
+        this.rank = rank;
+        this.title = title;
+        this.fullTitle = fullTitle;
+        this.year = year;
+        this.image = image;
+        this.crew = crew;
+        this.imDbRating = imDbRating;
+        this.imDbRatingCount = imDbRatingCount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFullTitle() {
+        return fullTitle;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getCrew() {
+        return crew;
+    }
+
+    public String getImDbRating() {
+        return imDbRating;
+    }
+
+    public String getImDbRatingCount() {
+        return imDbRatingCount;
+    }
+}
