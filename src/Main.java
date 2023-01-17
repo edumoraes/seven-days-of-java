1fdf9cf0d8e24c5e5d9f6e014f7a6525a6dc802e
Eduardo <edumoraesdg@gmail.com>
2023-01-17 17:39:51 -0400

    Alterando Main

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,13 @@
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        //HTTP request
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest
-                .newBuilder()
-                .uri(URI.create(System.getenv("URI_API")))
-                .GET()
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        //Parser
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        JsonNode responseNode = objectMapper
-                .readTree(response.body())
-                .path("items"); //parse to Jackson JsonNode
-
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); //Exception double quote
-
-        CollectionType collectionType = TypeFactory
-                .defaultInstance()
-                .constructCollectionType(List.class, Movie.class);
-
-        List<Movie> listMovies = objectMapper
-                .reader(collectionType)
-                .readValue(responseNode);
-
-
-
+        ImdbApiClient top250Movies = new ImdbApiClient("URI_API");
+        ImdbParser parser = new ImdbParser();
         HTMLGenerator html = new HTMLGenerator();
-
-        html.generate(listMovies);
-
-
-
+        html.generate(parser.generateList(top250Movies.response().body()));
     }
 }
